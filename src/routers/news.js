dac8df85ffc6ccad86560f745816153de8c21d5f
priyansh pandey <priyanshpandey2055@gmail.com>
2023-03-30 21:28:54 +0530

    news sorted

--- src/routers/news.js
+++ src/routers/news.js
@@ -3,19 +3,36 @@ const router = new express.Router();
 const inshorts = require('inshorts-news-api');
 
 router.get("/getnews", (req, res) => {
-
-    const { lang, category } = req.body;
-
+    const l = req.query.lang;
+    const c = req.query.category;
+    var options = {
+        lang: l,
+        category: c
+    }
     try {
-        var options = {
-            lang: lang,
-            category: category
-        }
-        inshorts.getNews(options, function (result) {
-            res.send(result).status(201);
+        inshorts.getNews(options, function (result, news_offset) {
+            res.send({ result: result, newId: news_offset }).status(201);
         });
     } catch (error) {
-        res.send(error);
+        res.send(error).status(400)
+    }
+});
+
+router.get("/getmorenews", (req, res) => {
+    const l = req.query.lang;
+    const c = req.query.category;
+    const id = req.query.id;
+    var options = {
+        lang: l,
+        category: c,
+        news_offset: id
+    }
+    try {
+        inshorts.getNews(options, function (result, news_offset) {
+            res.send({ result: result, newId: news_offset }).status(201);
+        });
+    } catch (error) {
+        res.send(error).status(400)
     }
 });
 

a91dbf2781d267ddce9cd2432c5610b783b282fa
pratayyy <2028026@kiit.ac.in>
2023-04-23 01:08:34 +0530

    weather api endpoint changes

--- src/controller/weather.js
+++ src/controller/weather.js
@@ -3,16 +3,19 @@ require("dotenv").config();
 const axios = require("axios");
 
 const getWeatherReport = expressAsyncHandler(async (req, res) => {
-    try {
-        const city = req.query.city;
-        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WEATHER_API_KEY}`)
-        res.send(data).status(200);
-    } catch (error) {
-        console.log("Error")
-        res.send(error).status(400);
-    }
-})
+  try {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`
+    );
+    res.send(data).status(200);
+  } catch (error) {
+    console.log("Error");
+    res.send(error).status(400);
+  }
+});
 
 module.exports = {
-    getWeatherReport,
-}
+  getWeatherReport,
+};
